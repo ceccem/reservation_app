eabc4be22c16df11f0ccacc23fa41e1d409cc57f
ceccem <117820613+ceccem@users.noreply.github.com>
2023-03-17 22:57:13 +0900

    編集２つに分けるやつ実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,18 +6,12 @@ class UsersController < ApplicationController
     
     def show
         @user = User.find_by(id: params[:id])
-        @room = Room.new
     end
 
     def profile
         @user = current_user
     end
 
-    def profupdate
-        @user = current_user
-        @user.update(params.require(:user).permit(:name, :email))
-        redirect_to "/"
-    end
 
 
 end
